f20c67c6c5a77e3948aaaa82f6327929c3833e37
Tachibana446 <stinger33z@gmail.com>
2016-12-04 23:44:45 +0900

    エコーするだけの状態

--- Program.cs
+++ Program.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Discord;
 
 namespace SinobigamiBot
 {
     class Program
     {
-        static void Main(string[] args)
+        // staticメソッドではbotを実行しない
+        static void Main(string[] args) => new Program().Start();
+
+        DiscordClient client = new DiscordClient();
+        IniFile ini = new IniFile();
+
+        public void Start()
         {
-            var ini = new IniFile();
-            var val = ini.GetValue(nameof(ini.TestSection),nameof(ini.TestValue));
+            var token = ini.GetValue("BotSetting", "Token");
+            if (token == null) throw new Exception("Tokenがiniファイル内に見つかりません");
+            var clientId = ini.GetValue("BotSetting", "ClientId");
+
+            if (clientId != null)
+                System.Diagnostics.Process.Start($"https://discordapp.com/api/oauth2/authorize?client_id={clientId}&scope=bot");
+
+            client.MessageReceived += async (s, e) =>
+            {
+                if (!e.Message.IsAuthor)
+                    await e.Channel.SendMessage(e.Message.Text);
+            };
+
+            client.ExecuteAndWait(async () => { await client.Connect(token, TokenType.Bot); });
         }
     }
 }
